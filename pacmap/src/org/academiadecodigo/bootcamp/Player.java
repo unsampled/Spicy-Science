93fa5109541ca50ec0db699b9d36a96d7417f6bf
AnnLuar <anamvs987@gmail.com>
2019-02-18 14:24:10 +0000

    add a dir to tests, add player walk test and resources root

---
+++ pacmap/src/org/academiadecodigo/bootcamp/Player.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 18/02/2019.
+ */
+public class Player {
+}
---
+++ pacmap/src/org/academiadecodigo/bootcamp/Tests/TestLab.java
@@ -0,0 +1,10 @@
+package org.academiadecodigo.bootcamp.Tests;
+
+import org.academiadecodigo.bootcamp.Player;
+
+public class TestLab {
+    public static void main(String[] args) {
+        Player p1 = new Player();
+
+    }
+}
