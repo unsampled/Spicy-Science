438c53e3b9bcaeb80058edb21cd26afd08d3903f
Tiago Soares <tlsoares@outlook.com>
2019-02-20 10:48:49 +0000

    Position hashCode updated

--- pacmap/src/org/academiadecodigo/bootcamp/Map.java
+++ pacmap/src/org/academiadecodigo/bootcamp/Map.java
@@ -6,41 +6,41 @@ import org.academiadecodigo.bootcamp.grid.Position;
 import java.util.HashMap;
 
 public class Map {
-    private HashMap<Position,Wall> walls;
+    private HashMap<Position, Wall> walls;
     private PacGrid pacGrid;
 
 
-    public Map(){
-        walls= new HashMap();
-        this.pacGrid = new PacGrid(53,31);
+    public Map() {
+        walls = new HashMap();
+        this.pacGrid = new PacGrid(53, 31);
         //pacGrid.init();
         outsideWalls();
     }
 
-private void outsideWalls () {
-    for (int i = 0; i <= pacGrid.getCols(); i++) {
+    private void outsideWalls() {
+        for (int i = 0; i <= pacGrid.getCols(); i++) {
 
-        Wall wall= new Wall(i, 0, pacGrid);
-        walls.put(wall.getPos(),wall);
-        Wall wall2 = new Wall(i,pacGrid.getRows(), pacGrid);
-        walls.put(wall2.getPos(),wall2);
+            Wall wall = new Wall(i, 0, pacGrid);
+            walls.put(wall.getPos(), wall);
+            Wall wall2 = new Wall(i, pacGrid.getRows(), pacGrid);
+            walls.put(wall2.getPos(), wall2);
+        }
+
+        for (int x = 1; x <= pacGrid.getRows() - 1; x++) {
+            Wall wall = new Wall(0, x, pacGrid);
+            walls.put(wall.getPos(), wall);
+            Wall wall2 = new Wall(pacGrid.getCols(), x, pacGrid);
+            walls.put(wall2.getPos(), wall2);
+        }
+
     }
-
-    for (int x = 1; x <= pacGrid.getRows()-1; x++) {
-        Wall wall = new Wall(0, x, pacGrid);
-        walls.put(wall.getPos(),wall);
-        Wall wall2 = new Wall( pacGrid.getCols(),x, pacGrid);
-        walls.put(wall2.getPos(),wall2);
-    }
-
-}
 
     public PacGrid getPacGrid() {
         return pacGrid;
     }
 
-public HashMap<Position, Wall> getWalls (){
-       return walls;
-}
+    public HashMap<Position, Wall> getWalls() {
+        return walls;
+    }
 
 }
